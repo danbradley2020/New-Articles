0fb7c447ad36efa5bbd8d951c3788f3547f9f49a
candycrushpro <bradleynilmar6702@gmail.com>
2020-04-28 08:37:34 +0300

    test for basics

---
+++ tests/test_basics.py
@@ -0,0 +1,20 @@
+import unittest
+from flask import current_app
+from app import create_app
+
+class BasicsTestCase(unittest.TestCase):
+    def setUp(self):
+        '''
+        Method called to prepare the test fixture. This is called immediately before calling the test method; other than AssertionError or SkipTest, any exception raised by this method will be considered an error rather than a test failure.
+        '''
+        self.app = create_app('development')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        '''
+        tearDown method that executes a set of instructions after every test in the whole app
+        tidies up after the test method has been run
+        '''
+
+        self.app_context.pop()
